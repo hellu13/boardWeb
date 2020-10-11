9c9c87c99c750b5ad4c18df2b5e7e7d2fa6ee276
kyungmo <hellu13@naver.com>
2020-10-12 08:44:46 +0900

    로그아웃 기능

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -67,6 +67,14 @@ public class DispatcherServlet extends HttpServlet {
 			
 		} else if(path.equals("/logout.do")) {
 			System.out.println("로그아웃 처리");
+			
+			// 1. 브라우저와 연결된 세션 객체를 강제 종료
+			HttpSession session = request.getSession();
+			session.invalidate();
+			
+			// 2. 세션 종료 후, 메인 화면으로 이동
+			response.sendRedirect("/insertBoard.do");
+			
 		} else if(path.equals("/insertBoard.do")) {
 			System.out.println("글 등록 처리");
 			
